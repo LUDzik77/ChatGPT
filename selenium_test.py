4cc7569d8ac7de3170034f425ea05ed0cf200ae7
Ludwik <ludwikpapaj@gmail.com>
2023-02-04 13:50:22 +0100

    test selenium

--- selenium_test.py
+++ selenium_test.py
@@ -5,16 +5,26 @@ url = "https://www.gpw.pl"
 
 driver = webdriver.Chrome()
 driver.get(url)
-page_source = driver.page_source
-print(page_source)
+
+div = driver.find_element("xpath", "//div[@class='table gpw_style margin-bottom-0']")
+text = div.text
+print(text)
 driver.close()
 
 workbook = openpyxl.Workbook()
 sheet = workbook.active
-sheet['A1'] = page_source
-
+sheet['A1'] = text
 
 workbook.save("content_of_the_website.xlsx")
+
+
+#page_source = driver.page_source
+##print(page_source)
+#driver.close()
+
+#workbook = openpyxl.Workbook()
+#sheet = workbook.active
+#sheet['A1'] = page_source
 
 
 
