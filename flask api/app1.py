fb41f58c1f1ba8b6ceae4984e31ad9ff138117a8
Ludwik <62941980+LUDzik77@users.noreply.github.com>
2023-04-25 14:49:15 +0200

    debug, venv

--- flask api/app1.py
+++ flask api/app1.py
@@ -1,4 +1,7 @@
 #run with venv venv1 activated
+#if run via bash (export+run)--> http://localhost:5000 instead of 127.0.0.1:6969 
+#it turns the run function and ignored the if __name__ etc
+
 from flask import Flask
 
 app = Flask(__name__)
@@ -6,3 +9,12 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     return("hello world")
+
+@app.route('/<_name>')
+def print_name(_name):
+    if app.debug == True:
+        return(f'Hello {_name}, we run app in debug mode')
+    return(f'Hello {_name}')
+
+if __name__ == "__main__":
+    app.run(port=6969, debug=True)
