ebc0424953d72979730290b4bda3a3f06f1d0ed2
Ludwik <62941980+LUDzik77@users.noreply.github.com>
2023-04-30 12:25:45 +0200

    form test

--- flask api/ppaFlask.py
+++ flask api/ppaFlask.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
@@ -18,7 +18,17 @@ def powitanie(imie):
 @app.route("/users/<int:id>")
 def info_user(id):
     return(f"Dane użytkownika o id {id}")
-           
+
+#we can  move it to sqllite db later on
+tasks = ["shopping", "cleaning"]
+ 
+@app.route("/todo", methods =["GET", "POST"])
+def todo():
+    #tasks = ["shopping", "cleaning"]
+    if request.method == "POST":
+        tasks.append(request.form['tasks'])
+    return render_template("todo.html", tasks=tasks)
+
 if __name__ == "__main__":
     app.run(port=6969)
     
